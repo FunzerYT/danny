f5291a2e0543cc2683cdae38801c39f40c95340b
Funzer <38872813+dannysdirc@users.noreply.github.com>
2022-11-14 01:55:26 -0800

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,26 @@
+/* ======================================================================
+  Author Custom JavaScript
+====================================================================== */
+// Loop through Array of Objects
+var objPeople = [
+	{ // Object @ 0 index
+		username: "admin",
+		password: "funzer"
+	}
+
+]
+
+function getInfo() {
+	var username = document.getElementById('username').value
+	var password = document.getElementById('password').value
+
+	for(var i = 0; i < objPeople.length; i++) {
+		// check is user input matches username and password of a current index of the objPeople array
+		if(username == objPeople[i].username && password == objPeople[i].password) {
+			window.open('stylish.html')
+			// stop the function if this is found to be true
+			return
+		}
+	}
+	console.log("incorrect username or password")
+}
